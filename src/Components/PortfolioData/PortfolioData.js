036efd297ac8217dc4126aebb0fd06bf99ce1fe7
matthewmahler <matthewjamesmahler@gmail.com>
2019-01-08 22:55:51 -0500

    nailed it

--- src/Components/PortfolioData/PortfolioData.js
+++ src/Components/PortfolioData/PortfolioData.js
@@ -49,7 +49,7 @@ const data = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius augue vestibulum nisl dignissim fringilla. Duis vestibulum purus sapien, nec ultricies enim maximus ac. Sed tincidunt tortor non dui imperdiet dignissim. Vestibulum vel mattis lectus. Integer dignissim ante libero, nec porttitor ex auctor sit amet."
   },
   {
-    title: "This Portfolio :)",
+    title: "This Portfolio",
     subTitle: "A Responsive React Portfolio",
     path: require("../../assets/you-are-here.png"),
     techList: ["React.js", "CSS Modules", "HTML5", "JSX", "Reactstrap"],
--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -14,19 +14,26 @@ class About extends Component {
             className={classes.mainSection}
           >
             <p className={classes.about}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              varius augue vestibulum nisl dignissim fringilla. Duis vestibulum
-              purus sapien, nec ultricies enim maximus ac. Sed tincidunt tortor
-              non dui imperdiet dignissim. Vestibulum vel mattis lectus. Integer
-              dignissim ante libero, nec porttitor ex auctor sit amet.
+              For the last 15 years I've eaten, slept and breathed music, from
+              playing in multiple bands to being an all around concert addict
+              and everything in between. Ultimately, this led me to Ramapo
+              College of New Jersey where I graduated in 2015 with a B.A. in
+              Music Production and Audio Engineering. after a few years of
+              freelancing around New Jersey it was time for a change.
             </p>
             <p className={classes.about}>
-              Nulla egestas venenatis tincidunt. Suspendisse ullamcorper augue
-              eget massa iaculis ultrices. Suspendisse potenti. Nunc consectetur
-              a nulla a commodo. Donec consequat in ligula sed blandit. Etiam eu
-              malesuada tellus, eget malesuada orci. Morbi consectetur libero
-              justo, vel pellentesque velit ultricies quis. Integer in fringilla
-              arcu. sapien nibh.
+              Realizing my passion for web development, I pivoted career paths,
+              enrolled in the Rutgers Coding Bootcamp for Full Stack
+              Development, and haven’t looked back. My current pet project is a
+              music streaming service geared towards unknown and unsigned local
+              bands built in modern javascript (
+              <span className={classes.mongo}>MongoDB</span>,
+              <span className={classes.express}> Express</span>,
+              <span className={classes.react}> React</span>,
+              <span className={classes.node}> Node</span>, and
+              <span className={classes.graph}> GraphQL</span>) but when I’m not
+              working on websites and web apps, you can usually find me in the
+              pit screaming every word of my favorite songs.
             </p>
           </Col>
           <Col
